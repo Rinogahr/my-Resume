22fd321d0d944ca1125fb3737fd09f85d1e765dc
Rinogahr <69148290+Rinogahr@users.noreply.github.com>
2022-07-15 16:48:45 -0300

    fsdf
    dsfdsfsd

--- src/componentes/right/right-component.js
+++ src/componentes/right/right-component.js
@@ -3,18 +3,31 @@ import RightStyle from '../right/right-component.module.css';
 import { TitleComponent } from '../title/title-component';
 import { InformationComponent } from '../information/information-component';
 import { BiUser } from "react-icons/bi";
+import { BiDesktop } from "react-icons/bi";
 
 function RightComponent (){
     return(
         <div className={RightStyle.containerRight}>
             <TitleComponent 
                 header="Rodrigo Pedro Maciel"
-                subheader="Web Developer"/> 
+                subheader="Jr Web Developer"/> 
             <InformationComponent
             infoIcon={<BiUser/>}
             infoTitle="Profile"
+            InfoText=
+                {
+                    `Programador com mais de 5 anos de experiência trabalhando na indústria de gestão escolar.
+                    Especializado em Node.Js, JavaScript, Angular.js, React.js, React Native,
+                    HTML, CSS, SQL, Figma, Pacote Office, Jquery , PHP, Laravel, Gith, GitHub, TypeScript,
+                    XAMP & WAMP server`
+                }
             />
-            <InformationComponent/>
+            <InformationComponent
+            infoIcon={<BiDesktop/>}
+            infoTitle="Work Experience"
+            infoLabel={false}
+            infoLabelText="NTDSOFT"
+            />
         </div>
     );
 }
